5a46e2611d6d0832eb80798eb21df5ee9a04c6a9
dnbnero <dnbneroph@gmail.com>
2021-03-03 12:18:59 +0300

    Trying to start from beginning. Now only creating array and filling with number

--- PrimeNumbers/Program.cs
+++ PrimeNumbers/Program.cs
@@ -1,42 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
 namespace SimpleNumbers
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Stopwatch stopWatch = new Stopwatch();
 
             Console.WriteLine("Ведите последнее число");
             var n_n = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine(" ");
-            int[] numbers = new int[n_n + 1];
-
-            //stopWatch.Start();
+            int[] numbers = new int[n_n + 1]; 
 
             for (int i = 0; i <= n_n; i++)
             {
                 numbers[i] = i;
-            }
-
-            //stopWatch.Stop();
-
-            for (int i = 0; i <= n_n; i++)
-            {
-                for (int b = 0; b <= n_n; b++)
-                {
-                    if (numbers[b] == 0 ^ numbers[i] == 0 ^ numbers[b] == 1 ^ numbers[i] == 1)
-                    {
-                        continue;
-                    }
-                    if (b * i <= n_n)
-                    {
-                        numbers[b * i] = 0;
-                    }
-
-                }
             }
 
             for (int i=0;i<=n_n;i++)
@@ -47,10 +24,6 @@ namespace SimpleNumbers
                 }
                 Console.Write(i + " ");
             }
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine();
-            //Console.WriteLine(stopWatch.Elapsed);
 
             while (true) ;
         }
