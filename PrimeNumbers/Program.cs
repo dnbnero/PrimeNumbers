d1db7309b955f7f2139e0cf368956abb512d74df
dnbnero <dnbneroph@gmail.com>
2021-03-03 11:14:29 +0300

    Добавьте файлы проекта.

---
+++ PrimeNumbers/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+namespace SimpleNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopWatch = new Stopwatch();
+
+            Console.WriteLine("Ведите последнее число");
+            var n_n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(" ");
+            int[] numbers = new int[n_n + 1];
+
+            //stopWatch.Start();
+
+            for (int i = 0; i <= n_n; i++)
+            {
+                numbers[i] = i;
+            }
+
+            //stopWatch.Stop();
+
+            for (int i = 0; i <= n_n; i++)
+            {
+
+            }
+
+            foreach (int i in numbers)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            //Console.WriteLine(stopWatch.Elapsed);
+
+            while (true) ;
+        }
+    }
+}
